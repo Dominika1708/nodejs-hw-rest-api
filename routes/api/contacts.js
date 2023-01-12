bf09fa07ed32ce352d33245036cf9b9dcdef7e59
Dominika Sosnowska <domi.sosnowska@o2.pl>
2023-01-12 22:36:30 +0100

    validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Joi = require('joi');
+const Joi = require("joi");
 const shortid = require("shortid");
 
 const {
@@ -12,29 +12,51 @@ const {
 
 const router = express.Router();
 
+const schemaPost = Joi.object({
+  id: Joi.string().required(),
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.string().required(),
+});
+
+const schemaPut = Joi.object({
+  name: Joi.string().min(3).max(30),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  phone: Joi.string(),
+});
+
 router.get("/", async (req, res, next) => {
   const contacts = await listContacts();
-  return res.status(200).json( contacts );
+  return res.status(200).json(contacts);
 });
 
 router.get("/:contactId", async (req, res, next) => {
   const id = req.params.contactId;
   const contact = await getContactById(id);
   return contact
-    ? res.status(200).json( contact )
+    ? res.status(200).json(contact)
     : res.status(404).json({ message: "Not found" });
 });
 
 router.post("/", async (req, res, next) => {
+  const id = shortid.generate();
+  schemaPost.validate({ id, ...req.body });
   const { name, email, phone } = req.body;
 
   if (!name || !email || !phone)
     return res.status(400).json({ message: "missing required name field" });
 
-  const id = shortid.generate();
   const contact = { id, name, email, phone };
   await addContact(contact);
-  return res.status(201).json( contact );
+  return res.status(201).json(contact);
 });
 
 router.delete("/:contactId", async (req, res, next) => {
@@ -50,13 +72,15 @@ router.delete("/:contactId", async (req, res, next) => {
 router.put("/:contactId", async (req, res, next) => {
   const id = req.params.contactId;
   const taskToEdit = await getContactById(id);
-  const {name, email, phone} = req.body;
+  schemaPut.validate(req.body);
+  const { name, email, phone } = req.body;
 
   if (!taskToEdit) return res.status(404).json({ message: "Not found" });
-  if (!name && !email && !phone) return res.status(400).json({ message: "missing fields" });
+  if (!name && !email && !phone)
+    return res.status(400).json({ message: "missing fields" });
 
   const newContact = await updateContact(id, req.body);
-  return res.status(200).json( newContact );
+  return res.status(200).json(newContact);
 });
 
 module.exports = router;
