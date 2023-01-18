bddc46dff758a6ecfdd76b8dbac331bfba52f177
Dominika Sosnowska <domi.sosnowska@o2.pl>
2023-01-18 17:53:13 +0100

    connected to mongodb

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require('./app')
+const { mongoose } = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+require("dotenv").config();
+
+const uriDb = process.env.DB_HOST;
+
+const connection = mongoose.connect(uriDb, {
+  promiseLibrary: global.Promise,
+  useUnifiedTopology: true,
+});
+
+connection
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("\nDatabase connection successful");
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(`Server not running. Error message: ${err.message}`);
+    process.exit(1);
+  });
